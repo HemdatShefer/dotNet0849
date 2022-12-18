4157d73bc40e630520e59f95a55145a916d1cf79
hemdat <hemdatse@gmail.com>
2022-12-18 21:17:24 +0200

    finish stage 2

--- BL/BlImplementation/Cart.cs
+++ BL/BlImplementation/Cart.cs
@@ -77,6 +77,8 @@ namespace BlImplementation
                 {
                     CheckProduct(orderItem);
                 }
+                    });
+                    });
 
             }
             catch
